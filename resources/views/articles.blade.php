c437093838043e2195f2e2d9ade7d56dbfe4f891
Redsed <sedratir@gmail.com>
2021-10-13 19:58:04 +0100

    Première version de app-mail laravel

---
+++ resources/views/articles.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+    <head>
+     <title>Articles</title>
+    </head>
+    <body>
+    <h1>Liste d'articles</h1>
+    </body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+
+Route::get('/',[PostController::class,'index']);
+/*Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('posts', function() {
+    return response()->json([
+     'title' => 'mon super titre',
+     'description' => 'ma super description'
+    ]);
+}
+);
+
+Route::get('/' , function() {
+   // return view('articles');
+});*/
